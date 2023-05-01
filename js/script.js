838b4f3d4bd75ebb0aac8041b799f7d1e003d9e6
Mustafa Hesham <66361736+mustafa3370@users.noreply.github.com>
2023-05-01 17:09:41 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,34 +4,32 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('active')
 };
-
-let slideIndex = 1;
+var slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides(n) { 
+  showSlides(slideIndex += n); 
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+var timer = setInterval(nextSlide, 3000); 
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
+function nextSlide() { 
+  plusSlides(1); 
+} 
+
+function showSlides(n) { 
+  var i; 
+  var slides = document.getElementsByClassName("mySlides"); 
+  var dots = document.getElementsByClassName("demo"); 
+  if (n > slides.length) {slideIndex = 1}    
+
+  if (n < 1) {slideIndex = slides.length} 
+  for (i = 0; i < slides.length; i++) { 
+     slides[i].style.display = "none";  
+  } 
+  for (i = 0; i < dots.length; i++) { 
+     dots[i].className = dots[i].className.replace(" active", ""); 
+  } 
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active"; 
+} 
